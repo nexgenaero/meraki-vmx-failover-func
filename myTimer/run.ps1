3c3894e5acb7f5ac2e625977f8f24f991197d839
Dominik Kopec <1305107+dkopec@users.noreply.github.com>
2025-06-06 18:09:30 -0400

    fix: add way to pass managed rersource group

--- myTimer/run.ps1
+++ myTimer/run.ps1
@@ -69,7 +69,7 @@ Write-Output -InputObject "HA NVA timer trigger function executed at: $(Get-Date
 $VMFW1Name = $env:FW1NAME      # Set the Name of the primary NVA firewall  
 $VMFW2Name = $env:FW2NAME      # Set the Name of the secondary NVA firewall  
 $FW1RGName = $env:FWRGNAME     # Set the ResourceGroup that contains FW1  
-$FW2RGName = $env:FWRGNAME     # Set the ResourceGroup that contains FW2  
+$FW2RGName = $env:FWRGNAME2    # Set the ResourceGroup that contains FW2  
 $Monitor = $env:FWMONITOR      # "VMStatus" or "TCPPort" are valid values  
 
 #--------------------------------------------------------------------------  
